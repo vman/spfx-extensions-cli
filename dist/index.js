f9494b0e0e3601666a02af464eb86d60c52697bc
vman <vardhaman.rd@gmail.com>
2017-08-21 17:17:34 +0100

    updated dist

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var program = require("Commander");
 var node_sp_auth_1 = require("node-sp-auth");
-var sp_pnp_js_1 = require("sp-pnp-js");
+var pnp = require("sp-pnp-js");
 var node_pnp_js_1 = require("node-pnp-js");
 program
     .version('0.1.0')
@@ -11,7 +11,6 @@ program
     .option('-s, --siteurl [siteurl]', 'SharePoint Online Site Url [null]', null)
     .parse(process.argv);
 if (program.connect) {
-    console.log(program.siteurl);
     if (!program.siteurl) {
         console.error('Please enter siteurl with --siteurl or -s');
         process.exit();
@@ -23,7 +22,7 @@ if (program.connect) {
         force: false,
         persist: true
     }).then(function () {
-        sp_pnp_js_1.setup({
+        pnp.setup({
             fetchClientFactory: function () {
                 return new node_pnp_js_1.default({
                     ondemand: true
@@ -33,12 +32,10 @@ if (program.connect) {
         });
     }).then(function () {
         // we need to use the Web constructor to ensure we have the absolute url
-        var web = new sp_pnp_js_1.Web(siteUrl_1);
+        var web = new pnp.Web(siteUrl_1);
+        //pnp.sp.profiles.myProperties.get("");
         web.select('Title').get().then(function (w) {
             console.log("Web's title: " + w.Title);
         });
     });
 }
-// if (program.pineapple) console.log('  - pineapple');
-// if (program.bbqSauce) console.log('  - bbq');
-// console.log('  - %s cheese', program.cheese); 
