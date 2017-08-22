6a407005394d5f32c5ebae74ebb735eed3aa4737
vman <vardhaman.rd@gmail.com>
2017-08-22 17:55:54 +0100

    switched to get permissions

--- dist/index.js
+++ dist/index.js
@@ -3,39 +3,31 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var program = require("Commander");
 var node_sp_auth_1 = require("node-sp-auth");
-var pnp = require("sp-pnp-js");
-var node_pnp_js_1 = require("node-pnp-js");
+var request = require("request-promise");
 program
     .version('0.1.0')
-    .option('-c, --connect', 'Connect to SharePoint Online')
-    .option('-s, --siteurl [siteurl]', 'SharePoint Online Site Url [null]', null)
+    .option('-p, --permissions', 'Show current user permissions')
     .parse(process.argv);
+var authHeaders;
+var siteUrl;
+/* Need preferences js */
 if (program.connect) {
-    if (!program.siteurl) {
-        console.error('Please enter siteurl with --siteurl or -s');
-        process.exit();
-    }
-    var siteUrl_1 = program.siteurl;
-    node_sp_auth_1.getAuth(siteUrl_1, {
+    siteUrl = program.connect;
+    node_sp_auth_1.getAuth(siteUrl, {
         ondemand: true,
         electron: require('electron'),
         force: false,
         persist: true
-    }).then(function () {
-        pnp.setup({
-            fetchClientFactory: function () {
-                return new node_pnp_js_1.default({
-                    ondemand: true
-                });
-            },
-            baseUrl: siteUrl_1
-        });
-    }).then(function () {
-        // we need to use the Web constructor to ensure we have the absolute url
-        var web = new pnp.Web(siteUrl_1);
-        //pnp.sp.profiles.myProperties.get("");
-        web.select('Title').get().then(function (w) {
-            console.log("Web's title: " + w.Title);
-        });
+    }).then(function (authResponse) {
+        authHeaders = authResponse.headers;
+        authHeaders.Accept = 'application/json;odata=nometadata';
+    });
+}
+if (program.permissions) {
+    request.get({
+        url: program.siteurl + "/_api/SP.Utilities.Utility.GetUserPermissionLevels",
+        headers: authHeaders
+    }).then(function (response) {
+        console.log(response);
     });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,40 @@
 #!/usr/bin/env node
 import * as program from 'Commander';
-import { getAuth } from 'node-sp-auth';
-import * as pnp from 'sp-pnp-js';
-import NodeFetchClient from 'node-pnp-js';
+import { getAuth, IAuthResponse } from 'node-sp-auth';
+import * as request from 'request-promise';
 
 program
   .version('0.1.0')
-  .option('-c, --connect', 'Connect to SharePoint Online')
-  .option('-s, --siteurl [siteurl]', 'SharePoint Online Site Url [null]', null)
+  //.option('-c, --connect <siteurl>', 'Connect to SharePoint Online', null)
+  .option('-p, --permissions', 'Show current user permissions')
   .parse(process.argv);
+
+let authHeaders: any;
+let siteUrl: string;
+
+/* Need preferences js */
 
 if (program.connect) {
 
-  if (!program.siteurl) {
-    console.error('Please enter siteurl with --siteurl or -s');
-    process.exit();
-  }
-
-  const siteUrl = program.siteurl;
+  siteUrl = program.connect;
   getAuth(siteUrl, {
     ondemand: true,
     electron: require('electron'),
     force: false,
     persist: true
-  }).then(() => {
-    pnp.setup({
-      fetchClientFactory: () => {
-        return new NodeFetchClient({
-          ondemand: true
-        });
-      },
-      baseUrl: siteUrl
-    });
-  }).then(() => {
-    // we need to use the Web constructor to ensure we have the absolute url
-    const web = new pnp.Web(siteUrl);
-    //pnp.sp.profiles.myProperties.get("");
-    web.select('Title').get().then((w) => {
-      console.log(`Web's title: ${w.Title}`);
-    });
+  }).then((authResponse: IAuthResponse) => {
+
+    authHeaders = authResponse.headers;
+    authHeaders.Accept = 'application/json;odata=nometadata';
+  });
+}
+
+if (program.permissions) {
+
+  request.get({
+    url: `${program.siteurl}/_api/SP.Utilities.Utility.GetUserPermissionLevels`,
+    headers: authHeaders
+  }).then((response) => {
+    console.log(response);
   });
 }
