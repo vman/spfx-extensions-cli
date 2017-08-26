09625df9bc1bec56efa81f1dbfc4153dc156883d
vman <vardhaman.rd@gmail.com>
2017-08-26 16:00:54 +0100

    added dist back in

---
+++ dist/enums/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ExtensionScope;
+(function (ExtensionScope) {
+    ExtensionScope["SiteCollection"] = "site";
+    ExtensionScope["Web"] = "web";
+})(ExtensionScope = exports.ExtensionScope || (exports.ExtensionScope = {}));
